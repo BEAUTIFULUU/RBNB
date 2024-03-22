d72eef805323d094c5858583bbfd7cb7774a1740
8EAUTIFUL <pawel.wardachpr@gmail.com>
2024-03-22 18:54:35 +0100

    KAN-12

--- visits/serializers.py
+++ visits/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from visits.models import Visit
+from visits.validators import validate_apartment_visit_date
 
 
 class VisitInputSerializer(serializers.Serializer):
-    date_time = serializers.DateTimeField()
+    date_time = serializers.DateTimeField(validators=[validate_apartment_visit_date])
 
 
-class VisitDetailOutputSerializer(serializers.ModelSerializer):
+class VisitOutputSerializer(serializers.ModelSerializer):
     class Meta:
         model = Visit
         fields = ["id", "apartment", "date_time", "state"]
--- visits/validators.py
+++ visits/validators.py
@@ -2,6 +2,8 @@ import uuid
 from datetime import datetime, timedelta
 from django.utils import timezone
 from rest_framework.exceptions import ValidationError
+
+from apartments.models import Apartment
 from visits.models import Visit
 
 
@@ -10,9 +12,12 @@ def validate_create_apartment_visit(apartment_id: uuid.uuid4, user_id: int) -> N
         raise ValidationError("Visit for this apartment already exists.")
 
 
-def validate_apartment_visit_date(date_time: datetime) -> None:
+def validate_apartment_visit_date(
+    date_time: datetime, user_id: int, apartment_id: uuid.uuid4()
+) -> None:
     current_time = timezone.now()
     max_allowed_date = current_time + timedelta(days=60)
+    apartment_obj = Apartment.objects.get(id=apartment_id)
 
     if date_time < current_time:
         raise ValidationError("Visit date must be in the future.")
@@ -21,3 +26,6 @@ def validate_apartment_visit_date(date_time: datetime) -> None:
         raise ValidationError(
             "Visit date cannot be more than 2 months from the current date."
         )
+
+    if apartment_obj.owner.id == user_id:
+        raise ValidationError("You cannot create visit for your own apartment.")
